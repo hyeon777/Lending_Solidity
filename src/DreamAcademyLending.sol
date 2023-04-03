dfe2117fcb23d6860d399ace17c87bfb51cdcdd5
hyeon777 <yeonghyeon.choi@dream-academy.io>
2023-04-04 01:16:41 +0900

    19/28 succeeded

--- src/DreamAcademyLending.sol
+++ src/DreamAcademyLending.sol
@@ -2,12 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-import "./IPriceOracle.sol";
+
+interface IPriceOracle {
+    function getPrice(address token) external view returns (uint256);
+    function setPrice(address token, uint256 price) external;
+}
 
 contract DreamAcademyLending {
     uint256 public number;
     ERC20 usdc;
     uint256 usdc_amount;
+    IPriceOracle IPO;
     mapping (address => uint256) loan_ledger;
     mapping (address => deposit_) deposit_ledger;
 
@@ -17,7 +22,7 @@ contract DreamAcademyLending {
     }
 
     constructor(IPriceOracle _oracle, address _usdc) {
-        IPriceOracle IPO = IPriceOracle(_oracle);
+        IPO = IPriceOracle(_oracle);
         usdc = ERC20(_usdc);
     }
     function initializeLendingProtocol(address _usdc_) public payable {
@@ -31,26 +36,31 @@ contract DreamAcademyLending {
         }
         else{
             require(amount > 0, "amount must be more than zero");
-            ERC20(tokenAddress).transferFrom(msg.sender, address(this), amount);
+            usdc.transferFrom(msg.sender, address(this), amount);
             deposit_ledger[msg.sender].usdc_amount += amount;
             usdc_update();
         }
 
     }
     function borrow(address tokenAddress, uint256 amount) external {
-        require(tokenAddress == address(usdc));
-        require(usdc.balanceOf(address(this)) >= amount);
+        require(tokenAddress == address(usdc), "please check your tokenAddress again");
+        require(usdc.balanceOf(address(this)) >= amount, "Too much usdc required");
         usdc.transfer(msg.sender, amount);
         loan_ledger[msg.sender] += amount;
+        usdc_update();
     }
     function repay(address tokenAddress, uint256 amount) external {
-        require(tokenAddress == address(usdc));
-        require(loan_ledger[msg.sender] >= amount);
+        require(tokenAddress == address(usdc), "please check your tokenAddress again");
+        require(loan_ledger[msg.sender] >= amount, "please check your token amount");
         ERC20(tokenAddress).transferFrom(msg.sender, address(this), amount);
         loan_ledger[msg.sender] -= amount;
+        usdc_update();
     }
     function liquidate(address user, address tokenAddress, uint256 amount) external {
-
+        require(amount>0, "amount should be more than zero");
+        uint ETH_price = IPO.getPrice(address(0));
+        uint usdc_price = IPO.getPrice(tokenAddress);
+        loan_ledger[msg.sender] -= amount;
     }
     function withdraw(address tokenAddress, uint256 amount) external {
         if(tokenAddress == address(0)){
@@ -62,6 +72,7 @@ contract DreamAcademyLending {
             require(deposit_ledger[msg.sender].usdc_amount >= amount);
             deposit_ledger[msg.sender].usdc_amount -= amount;
             usdc.transfer(msg.sender, amount);
+            usdc_update();
         }
     }
 
