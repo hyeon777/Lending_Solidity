1cca30ee5bd9f4a35641a6d0b5a7c6728589d871
hyeon777 <yeonghyeon.choi@dream-academy.io>
2023-04-06 02:36:18 +0900

    22/28 succeeded

--- src/DreamAcademyLending.sol
+++ src/DreamAcademyLending.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "forge-std/console.sol";
 
 interface IPriceOracle {
     function getPrice(address token) external view returns (uint256);
@@ -15,7 +16,6 @@ contract DreamAcademyLending {
     IPriceOracle IPO;
     mapping (address => uint256) loan_ledger;
     mapping (address => deposit_) deposit_ledger;
-
     struct deposit_{
         uint usdc_amount;
         uint eth_amount;
@@ -30,11 +30,11 @@ contract DreamAcademyLending {
         usdc_update();   
     }
     function deposit(address tokenAddress, uint256 amount) external payable{
-        if(tokenAddress == address(0)){
-            require(amount == msg.value && amount > 0, "error with value");
+        if(tokenAddress == address(0)){   //ETH Deposit
+            require(amount == msg.value && amount > 0, "error with your deposit amount");
             deposit_ledger[msg.sender].eth_amount += amount;
         }
-        else{
+        else{   //USDC Deposit
             require(amount > 0, "amount must be more than zero");
             usdc.transferFrom(msg.sender, address(this), amount);
             deposit_ledger[msg.sender].usdc_amount += amount;
@@ -44,17 +44,34 @@ contract DreamAcademyLending {
     }
     function borrow(address tokenAddress, uint256 amount) external {
         require(tokenAddress == address(usdc), "please check your tokenAddress again");
-        //require(usdc.balanceOf(address(this)) >= amount, "Too much usdc required");
+        require(usdc.balanceOf(address(this)) >= amount, "We don't have that much usdc required");
 
-        uint usdc_price = IPO.getPrice(address(0x0));
-        uint eth_price = IPO.getPrice(address(usdc));
-        uint borrow_enable = (deposit_ledger[msg.sender].eth_amount * usdc_price / eth_price / 2) - loan_ledger[msg.sender];
+        uint available_amount = borrow_amount(amount);
 
-        require(borrow_enable >= amount, "amount too much");
-
+        require(available_amount >= amount, "amount too much");
         usdc.transfer(msg.sender, amount);
         loan_ledger[msg.sender] += amount;
+
         usdc_update();
+    }
+    function borrow_amount(uint amount) public returns (uint) {
+        uint eth_price = IPO.getPrice(address(0x0));
+        uint usdc_price = IPO.getPrice(address(usdc));
+
+        uint deposit_value = EtherToUsdc(deposit_ledger[msg.sender].eth_amount);
+        return (deposit_value / 2) - loan_ledger[msg.sender];
+    }
+    function EtherToUsdc(uint amount) public returns (uint) {
+        uint eth_price = IPO.getPrice(address(0x0));
+        uint usdc_price = IPO.getPrice(address(usdc));
+
+        return amount * eth_price / usdc_price;
+    }
+    function UsdcToEther(uint amount) public returns (uint) {
+        uint eth_price = IPO.getPrice(address(0x0));
+        uint usdc_price = IPO.getPrice(address(usdc));
+
+        return amount * usdc_price / eth_price;
     }
     function repay(address tokenAddress, uint256 amount) external {
         require(tokenAddress == address(usdc), "please check your tokenAddress again");
@@ -63,19 +80,27 @@ contract DreamAcademyLending {
         loan_ledger[msg.sender] -= amount;
         usdc_update();
     }
-    function liquidate(address user, address tokenAddress, uint256 amount) external {
+    function liquidate(address user, address tokenAddress, uint256 amount) external payable{   //usdc로 단위로 모든 계산 통일
         require(amount>0, "amount should be more than zero");
-        uint ETH_price = IPO.getPrice(address(0));
-        uint usdc_price = IPO.getPrice(tokenAddress);
-        loan_ledger[msg.sender] -= amount;
+        uint ETH_Price = IPO.getPrice(address(0));
+        uint usdc_Price = IPO.getPrice(address(usdc));
+
+        require(EtherToUsdc(deposit_ledger[user].eth_amount)*75/100 <= loan_ledger[user], "liquidation is not available yet");
+        usdc.transferFrom(msg.sender, address(this), amount);
+        console.log(deposit_ledger[user].eth_amount*ETH_Price*75/100, UsdcToEther(loan_ledger[user]));
+        uint loan_amount = EtherToUsdc(loan_ledger[user]);
+        uint ETH_Amount = loan_ledger[user] * amount / loan_amount;
+
+        loan_ledger[user] -= amount;
+        msg.sender.call{value: ETH_Amount}("");
     }
     function withdraw(address tokenAddress, uint256 amount) external {
-        if(tokenAddress == address(0)){
+        if(tokenAddress == address(0)){  //ETH
             require(deposit_ledger[msg.sender].eth_amount >= amount);
             deposit_ledger[msg.sender].eth_amount -= amount;
             msg.sender.call{value: amount}("");
         }
-        else{
+        else{   //USDC
             require(deposit_ledger[msg.sender].usdc_amount >= amount);
             deposit_ledger[msg.sender].usdc_amount -= amount;
             usdc.transfer(msg.sender, amount);
